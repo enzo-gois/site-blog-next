c5597b11e1333172413e6d58e21c6cde6126d9f8
enzo gois <enzoab26@outlook.com>
2025-10-25 16:53:53 -0300

    feat: migrate homePage to app router

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { LandingPage } from "@/templates/landing-page";
+import "@/styles/globals.css";
+
+export default function HomePage() {
+  return (
+    <>
+      <LandingPage />
+    </>
+  );
+}

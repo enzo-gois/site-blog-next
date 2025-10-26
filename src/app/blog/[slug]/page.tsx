8420a65d75bde10ce6c61d5d3c2f0b730d73d274
enzo gois <enzoab26@outlook.com>
2025-10-26 17:10:55 -0300

    feat: add seo in blog post page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,13 +1,38 @@
+import type { Metadata } from "next";
 import { PostPage } from "@/templates/blog";
-import { Post } from "contentlayer.config";
 import { allPosts } from "contentlayer/generated";
 import { notFound } from "next/navigation";
 
-type BlogPostPage = {
+type BlogPostPageProps = {
   params: Promise<{
     slug: string;
   }>;
 };
+
+export async function generateMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
+
+  if (!post) {
+    return {};
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [
+      {
+        name: post.author.name,
+      },
+    ],
+    robots: "index, follow",
+    openGraph: {
+      images: [post.image],
+    },
+  };
+}
 
 export async function generateStaticParams() {
   return allPosts.map((post) => ({
@@ -15,7 +40,7 @@ export async function generateStaticParams() {
   }));
 }
 
-export default async function BlogPostPage({ params }: BlogPostPage) {
+export default async function BlogPostPage({ params }: BlogPostPageProps) {
   const { slug } = await params;
   const post = allPosts.find((post) => post.slug === slug);
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,22 @@ import "@/styles/globals.css";
 export const metadata: Metadata = {
   title: "Site.set",
   description: "Venda seus produtos como afiliado em um único lugar",
+  openGraph: {
+    title: "Site.set",
+    description: "Venda seus produtos como afiliado em um único lugar",
+    url: "https://site-blog-next-two.vercel.app/og-image.jpg",
+    siteName: "Site.set",
+    locale: "pt_BR",
+    type: "website",
+    images: [
+      {
+        url: "https://site-blog-next-two.vercel.app/og-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "Site.set",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({

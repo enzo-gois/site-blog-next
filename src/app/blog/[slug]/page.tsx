3ceb0d3be9ed0a0c847b3bf51d35475e56aa0e1a
enzo gois <enzoab26@outlook.com>
2025-10-25 22:12:57 -0300

    feat: postPage with app router

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,21 @@
+import { PostPage } from "@/templates/blog";
+import { Post } from "contentlayer.config";
+import { allPosts } from "contentlayer/generated";
+import { notFound } from "next/navigation";
+
+type BlogPostPage = {
+  params: Promise<{
+    slug: string;
+  }>;
+};
+
+export default async function BlogPostPage({ params }: BlogPostPage) {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
+
+  if (!post) {
+    notFound();
+  }
+
+  return <PostPage post={post} />;
+}
